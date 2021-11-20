66c2170a94e6dd3fc43150379cef2887b6dad52d
Suranga Handagala <suranga@northeastern.edu>
2021-11-20 11:59:50 +0000

    Build script updated

--- run_build.sh
+++ run_build.sh
@@ -4,8 +4,12 @@ if ! [ -v "XILINXD_LICENSE_FILE" ]; then
 	exit 1
 fi
 if [ "$#" -ne 3 ]; then
-	echo "Syntax: run_build.sh <build_sender=(1 or 0)> <build_receiver=1 or 0> <interface=0 or 1>"
-    exit 1
+	echo "Syntax: run_build.sh <build_sender=(1 or 0)> <build_receiver=1 or 0> <sender interface=0 or 1> <receiver interface=0 or 1>"
+    	exit 1
+fi
+if [ "$3" -ne "0" ] || ["$3" -ne "1" ]
+	echo "Syntax: run_build.sh <build_sender=(1 or 0)> <build_receiver=1 or 0> <sender interface=0 or 1> <receiver interface=0 or 1>"
+	exit 1
 fi
 if [ "$1" == "1" ]; then
 	echo "Building the sender"
@@ -13,14 +17,16 @@ if [ "$1" == "1" ]; then
 elif [ "$1" == "0" ]; then
 	echo "Not building the sender"
 else
-	echo "Syntax: run_build.sh <build_sender=(1 or 0)> <build_receiver=1 or 0> <interface=0 or 1>"
+	echo "Syntax: run_build.sh <build_sender=(1 or 0)> <build_receiver=1 or 0> <sender interface=0 or 1> <receiver interface=0 or 1>"
+	exit 1
 fi
 if [ "$2" == "1" ]; then
 	echo "Buildng the receiver"
-	make all USER_KRNL=hls_recv_krnl INTERFACE=$3
+	make all USER_KRNL=hls_recv_krnl INTERFACE=$4
 elif [ "$2" == "0" ]; then
         echo "Not building the receiver"
 else
-        echo "Syntax: run_build.sh <build_sender=(1 or 0)> <build_receiver=1 or 0> <interface=0 or 1>"
+        echo "Syntax: run_build.sh <build_sender=(1 or 0)> <build_receiver=1 or 0> <sender interface=0 or 1> <receiver interface=0 or 1>"
+	exit 1
 fi
 
